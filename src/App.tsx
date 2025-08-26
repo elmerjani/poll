ce4487c07cb89143716c0015dde26650c4c4ad7c
mohamedelmerjani <mohamedelmerjani3@gmail.com>
2025-08-26 07:10:37 +0100

    Add authentication with amazon cognito

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 import "./App.css";
+import { useAuth } from "react-oidc-context";
 
 function App() {
+  const auth = useAuth();
+  const signOutRedirect = () => {
+    const clientId = import.meta.env.VITE_COGNITO_CLIENT_ID;
+    const logoutUri = import.meta.env.VITE_COGNITO_REDIRECT_URI;
+    const cognitoDomain = import.meta.env.VITE_COGNITO_DOMAIN;
+    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+  };
 
+  if (auth.isLoading) return <div>Loading...</div>;
+  if (auth.error) return <div>Error: {auth.error.message}</div>;
+  
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div>
+      {auth.isAuthenticated ? (
+        <>
+          <h2>Welcome, {auth.user?.profile.email}</h2>
+          <p>ID Token: {auth.user?.id_token}</p>
+          <p>Access Token: {auth.user?.access_token}</p>
+          <button onClick={() => {
+            auth.removeUser();
+            signOutRedirect();
+          }}>Sign out</button>
+        </>
+      ) : (
+        <button onClick={() => auth.signinRedirect()}>Sign in</button>
+      )}
+    </div>
   );
 }
 
---
+++ src/components/providers/AuthProvider.tsx
@@ -0,0 +1,16 @@
+import type { ReactNode } from "react";
+import { AuthProvider } from "react-oidc-context";
+
+const cognitoAuthConfig = {
+  authority: import.meta.env.VITE_COGNITO_AUTHORITY,
+  client_id: import.meta.env.VITE_COGNITO_CLIENT_ID,
+  redirect_uri: import.meta.env.VITE_COGNITO_REDIRECT_URI,
+  response_type: import.meta.env.VITE_COGNITO_RESPONSE_TYPE,
+  post_logout_redirect_uri: import.meta.env.COGNITO_REDIRECT_URI,
+  scope: import.meta.env.VITE_COGNITO_SCOPE,
+};
+
+export default function Component({ children }: { children: ReactNode }) {
+  console.log(cognitoAuthConfig)
+  return <AuthProvider {...cognitoAuthConfig}>{children}</AuthProvider>;
+}
